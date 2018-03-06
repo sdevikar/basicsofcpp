acf945884d5503070ced6404f95f64730b407dad
Swapnil Devikar <swap1712@gmail.com>
2018-03-06 00:16:14 -0500

    added constructor_destructor exmples

---
+++ Sandbox/constructor-destructors/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "point.h"
+
+using namespace std;
+
+int main(void){
+  cout << "Creating Point instance"<<endl;
+  Point point(1,2);
+  cout << "Returning from main"<<endl;
+  return 0;
+}
---
+++ Sandbox/constructor-destructors/point.h
@@ -0,0 +1,21 @@
+#ifndef _POINT_H_
+#define _POINT_H_
+#include <iostream>
+
+using namespace std;
+
+class Point{
+private:
+  int x;
+  int y;
+  int init(int x);
+
+public:
+  Point(int x, int y);
+  ~Point();
+  int GetX();
+  int GetY();
+  void SetX(int x);
+  void SetY(int y);
+};
+#endif
