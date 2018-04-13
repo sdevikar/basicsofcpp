237b3aff922405ca5eb51d98d47170390a1df499
Swapnil Devikar <swap1712@gmail.com>
2018-04-12 23:26:34 -0400

    added iostream operator overriding examples

---
+++ examples/iostream_operators_for_udt/build/CMakeFiles/udt_operator_overloading.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/udt_operator_overloading.dir/main.cpp.o"
+  "udt_operator_overloading.pdb"
+  "udt_operator_overloading"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/udt_operator_overloading.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
